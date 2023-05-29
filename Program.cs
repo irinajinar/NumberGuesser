b6d0afe32e96353f4d3f3d0733aeb3bf668cfa81
Irina Jinar <irinajinar.simedru@gmail.com>
2023-05-29 15:42:42 +0300

    Added the NumberGuesser app

--- Program.cs
+++ Program.cs
@@ -1,15 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumberGuesser
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            GetAppInfo();
+            GreetUser();
+
+            
+            while (true)
+            {
+                // int correctNumber = 7;
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+                int guess = 0;
+                Console.WriteLine("Guess a number between 1 and 10!");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+                    if (!int.TryParse(input, out guess))
+                    {
+                       PrintColorMessage(ConsoleColor.Red, "Please use an actual number!");
+                        continue;
+                    }
+                    guess = Int32.Parse(input);
+                    if (guess != correctNumber)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again!");
+                        continue;
+                    }
+                }
+                
+                PrintColorMessage(ConsoleColor.Yellow, "You are correct!!!!");
+                Console.WriteLine("Play again? [Y or N]");
+
+                string answer= Console.ReadLine().ToUpper();
+                if(answer == "Y")
+                {
+                    continue;
+                }
+                else if(answer == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        static void GetAppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Irina Jinar";
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name?");
+            string inputName = Console.ReadLine();
+            Console.WriteLine("Hello {0}, let's play a game....", inputName);
+        }
+
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+
+            Console.ResetColor();
         }
     }
 }
